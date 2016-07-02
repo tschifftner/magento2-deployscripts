8fae30568b3f7c428f5446f73f19cd075a26e9f8
Tobias Schifftner <ts@ambimax.de>
2016-07-02 15:59:11 +0200

    Changed install.sh deployment to composer/bin

--- deploy.sh
+++ deploy.sh
@@ -157,8 +157,8 @@ fi
 # Step 3: Trigger installation
 ########################################################################################################################
 
-if [ ! -f "${RELEASEFOLDER}/bin/install.sh" ] ; then echo "Could not find installer" ; exit 1; fi
-${RELEASEFOLDER}/bin/install.sh -e "${ENVIRONMENT}" || { echo "Installing package failed"; exit 1; }
+if [ ! -f "${RELEASEFOLDER}/vendor/bin/install.sh" ] ; then echo "Could not find installer" ; exit 1; fi
+${RELEASEFOLDER}/vendor/bin/install.sh -e "${ENVIRONMENT}" || { echo "Installing package failed"; exit 1; }
 
 
 
